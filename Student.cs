58e8416f914fee4102270f8e056c156ffa1b0436
Nirajan1-droid <nirajanbhattarai12345@gmail.com>
2024-04-04 15:07:44 +0545

    Added or modified files from the 'Models' subfolder

---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Models
+{
+
+    [Table("StudentTable")]
+    public class Student
+    {
+
+        [Key]
+        public int id { get; set; } 
+        public string Name { get; set; }
+
+    }
+}
