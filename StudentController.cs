d761f4e5ba73d087762d244381be9d32d3517cb5
Nirajan1-droid <nirajanbhattarai12345@gmail.com>
2024-04-04 15:07:36 +0545

    Added or modified files from the 'Controllers' subfolder

---
+++ StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly YourDbContext _context;
+
+        public StudentController(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit(Student userInfo)
+        {
+            try
+            {
+                _context.StudentTables.Add(userInfo);
+                _context.SaveChanges();
+                return RedirectToAction("Detail", userInfo);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error saving student information: " + ex.Message);
+            }
+        }
+
+        public IActionResult Detail(Student userInfo)
+        {
+            return View(userInfo);
+        }
+    }
+}
