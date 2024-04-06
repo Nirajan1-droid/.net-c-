e6001b308e173d897584351aad8fff998cdb8656
Nirajan1-droid <nirajanbhattarai12345@gmail.com>
2024-04-06 21:00:17 +0545

    Added or modified files from the 'historychecker/net' directory

---
+++ Dot-Net-Programming/03-ASP-Dot-NET/02-College-Web-Apps/01-Form-Input-App/WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly YourDbContext _context;
+
+        public StudentController(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Submit(Student userInfo)
+        {
+            try
+            {
+                await _context.StudentTables1.AddAsync(userInfo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Detail", userInfo);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error saving student information: " + ex.Message);
+            }
+        }
+
+
+        public async Task<IActionResult> Detail(Student userInfo)
+        {
+            var studentsName = await _context.StudentTables1.ToListAsync();
+            await Console.Out.WriteLineAsync(studentsName.ToString());
+            return View(userInfo);
+        }
+    }
+}
