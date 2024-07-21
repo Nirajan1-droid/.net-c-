777464f2e48ae0c1f8c15567f0f53e4d0fc0097c
Nirajan <65585311+Nirajan1-droid@users.noreply.github.com>
2024-07-21 10:39:48 +0545

    final-migration (#2)
    this is the final migration for student MVC application
    
    Co-authored-by: Sudip Yadav <112951953+sudip7878@users.noreply.github.com>

---
+++ Migrations-second/20240404081151_AddStudentsTableToDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentsTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentTable",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentTable", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentTable");
+        }
+    }
+}
